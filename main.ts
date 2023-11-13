64c4a75bfe195526ad05b7d817e09b64d3fa8b76
MarcCrusellas <71900639+MarcCrusellas@users.noreply.github.com>
2023-11-13 19:22:13 +0100

    tot

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-
+let ran = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("pedra")
+})
+input.onGesture(Gesture.Shake, function () {
+    ran = randint(0, 2)
+    if (ran == 0) {
+        basic.showString("pedra")
+    }
+    if (ran == 1) {
+        basic.showString("paper")
+    }
+    if (ran == 2) {
+        basic.showString("tisores")
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("tisores")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("paper")
+})
